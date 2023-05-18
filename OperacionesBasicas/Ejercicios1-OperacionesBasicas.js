1aab2f54497e48bae660b9c553f4aeb9535df55d
PabloSanturio <pablosanturio1@gmail.com>
2023-05-18 14:14:49 -0300

    Operaciones Basicas - Serie 1 de ejercicios

---
+++ OperacionesBasicas/Ejercicios1-OperacionesBasicas.js
@@ -0,0 +1,36 @@
+//Operaciones basicas, Tipos de datos, prompt y Alert
+
+         // 1)   
+                a = parseInt(prompt("ingrese un numero"))
+                b = parseInt(prompt("ingrese otro numero"))
+        
+                alert(a + b) 
+
+
+        // 2) 
+
+         tempC = parseInt(prompt("Ingrese la temperatura en Celcius"))
+ 
+         tempF = (tempC * 1.8) + 32
+ 
+         console.log(tempF);
+ 
+         alert("la temperatura en Fahrenheit es " + tempF) 
+
+
+        // 3)
+
+
+
+          numUsuario = parseInt(prompt("ingrese el numero a dividir"))
+    
+          alert("el resultado al dividirlo entre 10 es " + numUsuario / 10) 
+
+
+        // 4)
+
+
+         a = [1, 2, 3]
+
+        alert("el tipo de dato de 'a' es " + typeof a) 
+
